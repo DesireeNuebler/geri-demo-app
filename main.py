45751b2b2648d8b80e6db4da62bc0580013edaf8
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-02 16:56:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ col1, col2 = st.columns([2, 1])
 with col1: 
     st.title("") 
 with col2: 
-    st.image("app/assets/LMU_Klinikum_Logo.jpg", width=800)
+    st.image("assets/LMU_Klinikum_Logo.jpg", width=800)
 
 
 st.title("GERI - A Demo App for Risk Prediction")
