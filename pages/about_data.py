aac8ddfc8da3aa8d89631793ec71d23f8b31109b
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-09 12:13:27 +0200

    Update about_data.py

--- pages/about_data.py
+++ pages/about_data.py
@@ -11,12 +11,9 @@ with col2:
  
 st.title("About the Data")
 
-st.markdown("The patients included in this study are a subgroup of participants in the “Munich Sarcopenia Registry” (MUSAR), an ongoing registry study aimed at identifying modifiable risk factors and the underlying pathophysiological mechanisms of sarcopenia. Below you can see an overview of the patients characteristics with respect to the expert based predictors.")
-# with open("assets/spider_plot.html", "r", encoding="utf-8") as f:
-#     html = f.read()
-
-# components.iframe(srcdoc=html, height=500)
-
+st.markdown("The patients included in this study are a subgroup of participants in the “Munich Sarcopenia Registry” (MUSAR),\
+an ongoing registry study aimed at identifying modifiable risk factors and the underlying pathophysiological mechanisms of sarcopenia.\
+Below you can see an overview of the patients characteristics with respect to the expert based predictors.")
 
 
 with open("assets/spider_plot.html", "r", encoding="utf-8") as f:
