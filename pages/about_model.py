f92879985ddb8addd198a6474f7384b91e73beda
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-09 13:19:35 +0200

    Update about_model.py

--- pages/about_model.py
+++ pages/about_model.py
@@ -18,7 +18,7 @@ if st.button("Visualize the Random Forest Idea.")
 
 st.subheader("Explainability")
 st.markdown("For explainability **SHAP values** have been generated.The idea of SHAP values is to break down any machine learning model predictions and explain the contribution\
-             of each feature. Coming from game theory, each feature is allocated an importance value for a specific prediction, \
+             of each feature. Coming from game theory, each feature is allocated an importance value for a specific prediction,
             showing how much that feature pushed the result away from the average prediction. SHAP values of the trained model enable insights to the learned model structure, the SHAP value of a single prediction allows a deeper understanding of specific patient characteristics with respect to the learned model behaviour.")
 
 
