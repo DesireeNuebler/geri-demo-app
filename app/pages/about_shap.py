4dd74ef72c8d003f9c5802a6c100942138ec3104
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-09 14:45:10 +0200

    Update about_shap.py

--- app/pages/about_shap.py
+++ app/pages/about_shap.py
@@ -1,6 +1,7 @@
 'SHAP FOR SPECIFIC PREDICTION PAGE'
 
 import streamlit as st
+import shap
 
 col1, col2 = st.columns([2, 1]) 
 with col1: 
@@ -9,6 +10,19 @@ with col2:
     st.image("assets/LMU_Klinikum_Logo.jpg", width=800)
 
 st.title("Feature Contribution for the Patients Prediction")
+
+
+
+with open("assets/model.pkl", "rb") as f: 
+    model = pickle.load(f)
+explainer = shap.Explainer(model,)
+shap_values = explainer(X_test)
+# Compute SHAP values
+shap_values_pos = shap_values[:, :, 1]  
+shap_values = explainer(X_test)
+
+# Select one observation (e.g., index 0)
+shap.plots.waterfall(shap_values_pos[0])
 
 
 if st.button("Understand feature contribution in the whole model."): 
