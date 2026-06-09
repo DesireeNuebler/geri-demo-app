9b5d2ef7d3bc622688299748eafdeef8e5cc264f
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-09 11:51:27 +0200

    Update about_model.py

--- pages/about_model.py
+++ pages/about_model.py
@@ -8,21 +8,13 @@ with col1:
 with col2: 
     st.image("assets/LMU_Klinikum_Logo.jpg", width=800)
  
-st.title("About the Model: Support Vector Machines")
-
+st.title("About the Model: Random Forest Model")
 
 st.subheader("The Model")
-st.markdown("The trained model is a **Support Vector Machine**.\
-             The model tries to find a complex function in a higher dimensional feature space that seperates the two class,i.e. resistant and not resistant.\
-             In contrast to many other models SVM exploits geometry in finding a good decision function.\
-             The **kernel trick** involves the transformation from the orignal feature space into a higher dimensional feature space.\
-             In this higher dimensional feature space the classes are linearly seperatable.\
-             \n\nAssuming complex non linear relationships, models like the classical logistic regression fail to seperate the two classes:")
+st.markdown("The trained model is a **Random Forest Model**. Assuming complex non linear relationships, models like the classical logistic regression fail to seperate the two classes." \
+"\Machine learning models overcome this restriction and handle correlated predictors gracefully. " \
+"The basic concept of a random forest model is a decision tree that tries to find a complex function to seperate the patients into GH-sensitive and GH-resistant by sequently learing decision rules.")
 st.image("assets/log_reg_illustration.jpeg", width=350)
-st.markdown("In constrast, the SVM kernel trick solves the problem using transformation.")
-if st.button("Illustrate SVM kernel trick."):
-    st.switch_page("pages/about_kerneltrick.py")
-
 
 
 st.subheader("Explainability")
@@ -37,4 +29,3 @@ if st.button("Understand SHAP values of the trained model."):
 
 if st.button("⬅ Back"): 
     st.switch_page("main.py")
-
