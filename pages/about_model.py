fa6fe4eddc0cd5a8531fcb403c3dc74a0e2242db
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-10 10:13:23 +0200

    Update about_model.py

--- pages/about_model.py
+++ pages/about_model.py
@@ -11,15 +11,15 @@ with col2:
 st.title("The Random Forest Model")
 
 st.subheader("The Model")
-st.markdown("The trained model is a **Random Forest Model**.Assuming complex non linear relationships, models like the classical logistic regression fail to seperate the two classes. Machine learning models overcome this restriction and handle correlated predictors gracefully. The basic concept of a random forest model is a decision tree that tries to find a complex function to seperate the patients into GH-sensitive and GH-resistant by sequently learing decision rules. This idea is repeated multiple times resulting in a model consisting a various independent trees with each one voting for a class given the input.")
+# st.markdown("The trained model is a **Random Forest Model**.Assuming complex non linear relationships, models like the classical logistic regression fail to seperate the two classes. Machine learning models overcome this restriction and handle correlated predictors gracefully. The basic concept of a random forest model is a decision tree that tries to find a complex function to seperate the patients into GH-sensitive and GH-resistant by sequently learing decision rules. This idea is repeated multiple times resulting in a model consisting a various independent trees with each one voting for a class given the input.")
 
 if st.button("Visualize the Random Forest Idea.")
     st.switch_page("pages/about_randomforest.py")
 
 st.subheader("Explainability")
-st.markdown("For explainability **SHAP values** have been generated.The idea of SHAP values is to break down any machine learning model predictions and explain the contribution\
-             of each feature. Coming from game theory, each feature is allocated an importance value for a specific prediction,
-            showing how much that feature pushed the result away from the average prediction. SHAP values of the trained model enable insights to the learned model structure, the SHAP value of a single prediction allows a deeper understanding of specific patient characteristics with respect to the learned model behaviour.")
+# st.markdown("For explainability **SHAP values** have been generated.The idea of SHAP values is to break down any machine learning model predictions and explain the contribution\
+#              of each feature. Coming from game theory, each feature is allocated an importance value for a specific prediction,
+#             showing how much that feature pushed the result away from the average prediction. SHAP values of the trained model enable insights to the learned model structure, the SHAP value of a single prediction allows a deeper understanding of specific patient characteristics with respect to the learned model behaviour.")
 
 
 if st.button("Understand SHAP values of the trained model."): 
