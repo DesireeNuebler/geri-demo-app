69f55e4c9fe51d4b2182d24c8d17d58369c4e3dd
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-06-10 11:11:22 +0200

    test explainer import

--- pages/about_shap.py
+++ pages/about_shap.py
@@ -14,16 +14,16 @@ with col2:
 st.title("Feature Contribution for the Patients Prediction")
 
 
-age = st.session_state["age"] 
-sex = st.session_state["sex"] 
-igf1 = st.session_state["igf1"] 
-cortisol = st.session_state["cortisol"]
-t3 = st.session_state["t3"] 
-homa = st.session_state["homa"] 
-gh = st.session_state["gh"] 
-hghbp = st.session_state["hghbp"] 
-insulin = st.session_state["insulin"] 
-bmi = st.session_state["bmi"] 
+# age = st.session_state["age"] 
+# sex = st.session_state["sex"] 
+# igf1 = st.session_state["igf1"] 
+# cortisol = st.session_state["cortisol"]
+# t3 = st.session_state["t3"] 
+# homa = st.session_state["homa"] 
+# gh = st.session_state["gh"] 
+# hghbp = st.session_state["hghbp"] 
+# insulin = st.session_state["insulin"] 
+# bmi = st.session_state["bmi"] 
 
 # st.markdown("### Patient Characteristics")
 # cols = st.columns(2)
@@ -45,19 +45,19 @@ bmi = st.session_state["bmi"]
 #     with cols[i % 2]:
 #         st.metric(label=name, value=value)
 
-female =  1 if sex == "Female" else 0
+# female =  1 if sex == "Female" else 0
 
-X = [[female, bmi, insulin, hghbp, homa, gh, t3, cortisol,igf1, age]] # order in training
-skewed = ["insulin", "gh", "cortisol", "igfi", "homa"]
-X_scaled =  np.log1p(X)
+# X = [[female, bmi, insulin, hghbp, homa, gh, t3, cortisol,igf1, age]] # order in training
+# skewed = ["insulin", "gh", "cortisol", "igfi", "homa"]
+# X_scaled =  np.log1p(X)
 
 with open("assets/explainer.pkl", "rb") as f: 
     explainer = pickle.load(f) # trained explainer
-shap_values = explainer(X_scaled)
-shap_values_pos = shap_values[:, :, 1]  
+# shap_values = explainer(X_scaled)
+# shap_values_pos = shap_values[:, :, 1]  
 #shap.plots.waterfall(shap_values_pos)
-print(shap_values)
-
+ # print(shap_values)
+print(explainer)
 
 if st.button("Understand feature contribution in the whole model."): 
     st.switch_page("pages/about_shap_gen.py")
