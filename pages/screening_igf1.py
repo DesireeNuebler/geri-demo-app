d067ca98ed9bfc517f44c9b8603159ae3a54b743
DesireeNuebler <desiree.nuebler@med.uni-muenchen.de>
2026-08-26 13:18:22 +0000

    fix layout

--- pages/screening_igf1.py
+++ pages/screening_igf1.py
@@ -24,10 +24,10 @@ st.markdown("**2. IGF-I:**")
 col1, col2 = st.columns([2, 1])
 
 with col1:
-    has_igf1_deficient =st.radio("Does your patient suffer from IGF1 deficient?", options=["Please select...","Yes","No","I am not sure."])
+    has_igf1_deficient =st.radio("Does your patient suffer from IGF-I deficient?", options=["Please select...","Yes","No","I am not sure."])
 
 with col2:    
-    st.info("IGF1 Deficiency:  \nFemale: <67 ng/ml  \nMale: <86 ng/ml  \ndefinition based on: [Bidlingmaier et al., 2014](https://pubmed.ncbi.nlm.nih.gov/24606072/)", width=210)
+    st.info("IGF-I Deficiency:  \nFemale: <67 ng/ml  \nMale: <86 ng/ml  \n  \n  \ndefinition is based on: [Bidlingmaier et al., 2014](https://pubmed.ncbi.nlm.nih.gov/24606072/)", width=210)
 
 if has_igf1_deficient == "Please select...":
         st.warning("No choice has been made.", width=210)
